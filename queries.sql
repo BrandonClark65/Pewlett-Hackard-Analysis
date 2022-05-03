21d53259682c68ad06929591234193b8519d7a30
Brandon Clark <clark.brandon.98@gmail.com>
2022-05-03 14:55:29 -0700

    dept_info and manager_info

--- queries.sql
+++ queries.sql
@@ -106,3 +106,17 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 	AND (de.to_date = '9999-01-01');
 
 
+-- List of managers per department
+SELECT  dm.dept_no,
+        d.dept_name,
+        dm.emp_no,
+        ce.last_name,
+        ce.first_name,
+        dm.from_date,
+        dm.to_date
+INTO manager_info
+FROM dept_manager AS dm
+    INNER JOIN departments AS d
+        ON (dm.dept_no = d.dept_no)
+    INNER JOIN current_emp AS ce
+        ON (dm.emp_no = ce.emp_no);
