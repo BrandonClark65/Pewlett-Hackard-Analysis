50148a238b20802669edef8965b5faa4cbef8e12
Brandon Clark <clark.brandon.98@gmail.com>
2022-05-04 14:00:03 -0700

    mentorship_eligibility

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -30,3 +30,22 @@ INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY count DESC;
+
+-- Create a mentorship eligibility table 
+SELECT DISTINCT ON (e.emp_no) 
+    e.emp_no,
+    e.first_name,
+    e.last_name,
+    e.birth_date,
+    d.from_date,
+    d.to_date,
+    t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS d
+ON (e.emp_no = d.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (d.to_date = '9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
